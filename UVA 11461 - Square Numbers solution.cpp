4e6266e0cd761eaabdcd433597ff33859ec405d4
Ayman Mostafa <ayman93live@hotmail.com>
2016-03-17 19:07:19 +0200

    Create UVA 11461 - Square Numbers solution.cpp

---
+++ UVA 11461 - Square Numbers solution.cpp
@@ -0,0 +1,62 @@
+//============================================================================
+// Name        : ayman.cpp
+// Author      : Ayman Mostafa
+// Email       : ayman93live@hotmail.com
+// Version     : v5.0
+//============================================================================
+
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+
+using namespace std;
+
+#define all(x) x.begin(),x.end()
+#define allr(x) x.rbegin(),x.rend()
+
+void fastInOut();
+//##########################################################################################
+//###################################MAIN FUNCTION##########################################
+//##########################################################################################
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "rt", stdin);
+//freopen("output.txt", "wt", stdout);
+#endif
+	fastInOut();
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+// 11461 - Square Numbers
+ double x;
+ int a,s,ss;
+ while(cin>>s>>ss&&s&&ss){
+	 int c=0;
+	 for(int i=s;i<=ss;i++){
+		 x=sqrt(i);
+		 a=sqrt(i);
+		 if(x==a) c++;
+	 }
+	 cout<<c<<endl;
+ }
+}
+void fastInOut() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL), cout.tie(NULL);
+}
